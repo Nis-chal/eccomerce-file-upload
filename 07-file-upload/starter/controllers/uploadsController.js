9554d196cb48c7b8d538cb58b026c3f65decc727
Nis-chal <nischalchan@gmail.com>
2022-04-14 20:40:19 +0545

    now user can only upload images less than 1.24 mb

--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -3,7 +3,17 @@ const path = require("path");
 const { StatusCodes } = require("http-status-codes");
 
 const uploadProductImage = async (req, res) => {
-  let productImage = req.files.image;
+  if(!req.files){
+    throw new CustomError.BadRequestError('No Flie Uploaded')
+  }
+  const productImage = req.files.image;
+  if(!productImage.mimetype.startWith('image')){
+    throw new CustomError.BadRequestError('No file uploaded')
+  }
+  const maxSize = 1000 * 1024
+  if(productImage.size > maxSize){
+    throw new CustomError.BadRequestError('please upload image less than 1kb')
+  }
   console.log(req.files);
   const imagePath = path.join(
     __dirname,
