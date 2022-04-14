9ea2132c90884aefa10c6de09887d39863de9003
Nis-chal <nischalchan@gmail.com>
2022-04-14 20:28:40 +0545

    file upload can be done now

--- 07-file-upload/starter/app.js
+++ 07-file-upload/starter/app.js
@@ -4,9 +4,12 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const fileUpload = require('express-fileupload')
+
 // database
 const connectDB = require('./db/connect');
-
+//product router 
+const productRouter = require('./routes/productRoutes')
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -14,12 +17,18 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 app.get('/', (req, res) => {
   res.send('<h1>File Upload Starter</h1>');
 });
+app.use(express.static('./public'))
 
+app.use(express.json()) // built in middleware which is wee need  to use to have access to all the data from req.body
+app.use(fileUpload()) // you will have access to file in req.files
+app.use('/api/v1/products',productRouter)
 // middleware
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
+
+
 
 const start = async () => {
   try {
--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,14 +1,20 @@
-const Product = require("../models/Product");
+const path = require("path");
+
 const { StatusCodes } = require("http-status-codes");
 
 const uploadProductImage = async (req, res) => {
-  res.send("upload product image");
-};
-const getAllProducts = async (req, res) => {
-  res.send("create product");
+  let productImage = req.files.image;
+  console.log(req.files);
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads/" + `${productImage.name}`
+  );
+  await productImage.mv(imagePath);
+  return res.status(StatusCodes.OK).json({image:{src:`/uploads/${productImage.name}`}})
+
+ 
 };
 
 module.exports = {
-  createProduct,
-  getAllProducts,
+  uploadProductImage,
 };
