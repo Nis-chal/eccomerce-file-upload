001e2c506e39ed593208427a5ed8e5e71a0e9ccc
Nis-chal <nischalchan@gmail.com>
2022-04-15 11:15:33 +0545

    file upload is done with help of cloudinary

--- 07-file-upload/starter/app.js
+++ 07-file-upload/starter/app.js
@@ -1,34 +1,41 @@
-require('dotenv').config();
-require('express-async-errors');
+require("dotenv").config();
+require("express-async-errors");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const fileUpload = require('express-fileupload')
+const fileUpload = require("express-fileupload");
+
+//use v2
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 // database
-const connectDB = require('./db/connect');
-//product router 
-const productRouter = require('./routes/productRoutes')
+const connectDB = require("./db/connect");
+//product router
+const productRouter = require("./routes/productRoutes");
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.get('/', (req, res) => {
-  res.send('<h1>File Upload Starter</h1>');
+app.get("/", (req, res) => {
+  res.send("<h1>File Upload Starter</h1>");
 });
-app.use(express.static('./public'))
+app.use(express.static("./public"));
 
-app.use(express.json()) // built in middleware which is wee need  to use to have access to all the data from req.body
-app.use(fileUpload()) // you will have access to file in req.files
-app.use('/api/v1/products',productRouter)
+app.use(express.json()); // built in middleware which is wee need  to use to have access to all the data from req.body
+app.use(fileUpload({ useTempFiles: true })); // you will have access to file in req.files
+app.use("/api/v1/products", productRouter);
 // middleware
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
-
-
 
 const start = async () => {
   try {
--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,18 +1,20 @@
 const path = require("path");
 
 const { StatusCodes } = require("http-status-codes");
-
-const uploadProductImage = async (req, res) => {
-  if(!req.files){
-    throw new CustomError.BadRequestError('No Flie Uploaded')
+const CustomError = require("../errors");
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
+const uploadProductImageLocal = async (req, res) => {
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No Flies Uploaded");
   }
   const productImage = req.files.image;
-  if(!productImage.mimetype.startWith('image')){
-    throw new CustomError.BadRequestError('No file uploaded')
+  if (!productImage.mimetype.startWith("image")) {
+    throw new CustomError.BadRequestError("No file uploaded");
   }
-  const maxSize = 1000 * 1024
-  if(productImage.size > maxSize){
-    throw new CustomError.BadRequestError('please upload image less than 1kb')
+  const maxSize = 1000 * 1024;
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError("please upload image less than 1kb");
   }
   console.log(req.files);
   const imagePath = path.join(
@@ -20,9 +22,22 @@ const uploadProductImage = async (req, res) => {
     "../public/uploads/" + `${productImage.name}`
   );
   await productImage.mv(imagePath);
-  return res.status(StatusCodes.OK).json({image:{src:`/uploads/${productImage.name}`}})
-
- 
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
+};
+const uploadProductImage = async (req, res) => {
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    {
+      use_filename: true,
+      folder: "file-upload",
+    }
+  );
+  fs.unlinkSync(req.files.image.tempFilePath);
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${result.secure_url}` } });
 };
 
 module.exports = {
